83e8015f6e645a3ddb88291b2423c05d8e8578dd
BenjCH <benjamin.choron@outlook.com>
2024-07-20 23:20:05 +0200

    🚧 Create shopping item models and data

---
+++ lib/data/categories.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:smart_pantry/models/category.dart';
+
+const categories = {
+  Categories.fruit: Category(
+    name: 'Fruit',
+    icon: Icons.local_florist,
+    color: Color(0xFFF44336),
+    screen: Placeholder(),
+  ),
+  Categories.vegetable: Category(
+    name: 'Vegetable',
+    icon: Icons.eco,
+    color: Color(0xFF4CAF50),
+    screen: Placeholder(),
+  ),
+  Categories.meat: Category(
+    name: 'Meat',
+    icon: Icons.local_dining,
+    color: Color(0xFF795548),
+    screen: Placeholder(),
+  ),
+  Categories.seafood: Category(
+    name: 'Seafood',
+    icon: Icons.water,
+    color: Color(0xFF03A9F4),
+    screen: Placeholder(),
+  ),
+  Categories.dairy: Category(
+    name: 'Dairy',
+    icon: Icons.local_mall,
+    color: Color(0xFFFFEB3B),
+    screen: Placeholder(),
+  ),
+  Categories.bakery: Category(
+    name: 'Bakery',
+    icon: Icons.breakfast_dining,
+    color: Color(0xFF9C27B0),
+    screen: Placeholder(),
+  ),
+  Categories.drinks: Category(
+    name: 'Drinks',
+    icon: Icons.local_drink,
+    color: Color(0xFF009688),
+    screen: Placeholder(),
+  ),
+};
---
+++ lib/models/category.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  fruit,
+  vegetable,
+  meat,
+  seafood,
+  dairy,
+  bakery,
+  drinks,
+}
+
+class Category {
+  const Category({
+    required this.name,
+    required this.icon,
+    required this.color,
+    required this.screen,
+  });
+
+  final String name;
+  final IconData icon;
+  final Color color;
+  final Widget screen;
+}
