0411224d72eeb9a66b62e7a815d4d6abc0b7d35a
BenjCH <benjamin.choron@outlook.com>
2024-07-20 23:46:11 +0200

    🚧 Add shopping item form

---
+++ lib/widgets/category_dropdown_form_input.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'package:smart_pantry/data/categories.dart';
+import 'package:smart_pantry/models/category.dart';
+
+class CategoryDropdownFormInput extends StatefulWidget {
+  const CategoryDropdownFormInput({
+    super.key,
+    required this.category,
+    required this.onChanged,
+  });
+
+  final Category category;
+  final Function(Category) onChanged;
+
+  @override
+  State<CategoryDropdownFormInput> createState() =>
+      _CategoryDropdownFormInputState();
+}
+
+class _CategoryDropdownFormInputState extends State<CategoryDropdownFormInput> {
+  Category _selectedCategory = categories[Categories.fruits]!;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedCategory = widget.category;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField(
+      value: _selectedCategory,
+      dropdownColor: Theme.of(context).colorScheme.surfaceBright,
+      decoration: const InputDecoration(
+        border: InputBorder.none,
+      ),
+      style: TextStyle(
+        color: Theme.of(context).colorScheme.onSurface,
+      ),
+      items: categories.entries
+          .map((entry) => DropdownMenuItem(
+                value: entry.value,
+                child: Text(entry.value.name),
+              ))
+          .toList(),
+      onChanged: (value) {
+        setState(() {
+          _selectedCategory = value!;
+        });
+        widget.onChanged(value!);
+      },
+    );
+  }
+}
