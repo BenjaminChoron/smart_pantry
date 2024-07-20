5c08158e09a41bbbf4f48618c5fb6a6570ef196a
BenjCH <benjamin.choron@outlook.com>
2024-07-20 23:46:56 +0200

    🚧 Display shopping list items

--- lib/screens/shopping_list.dart
+++ lib/screens/shopping_list.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ShoppingListScreen extends StatelessWidget {
+import 'package:smart_pantry/providers/user_shopping_list.dart';
+
+class ShoppingListScreen extends ConsumerStatefulWidget {
   const ShoppingListScreen({super.key});
 
   @override
+  ConsumerState<ShoppingListScreen> createState() => _ShoppingListScreenState();
+}
+
+class _ShoppingListScreenState extends ConsumerState<ShoppingListScreen> {
+  late Future<void> _allItems;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _allItems = ref.watch(userShoppingListProvider.notifier).loadItems();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Shopping List',
-          style: TextStyle(color: Theme.of(context).colorScheme.onSurface)),
+    final allItems = ref.watch(userShoppingListProvider);
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+            future: _allItems,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(child: CircularProgressIndicator())
+                    : allItems.isEmpty
+                        ? const Center(
+                            child: Text(
+                              'No items in the shopping list',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          )
+                        : ListView.builder(
+                            itemCount: allItems.length,
+                            itemBuilder: (context, index) {
+                              final item = allItems[index];
+                              return ListTile(
+                                leading: CircleAvatar(
+                                  child: Text(item.category.name),
+                                ),
+                                title: Text(
+                                    '${item.quantity} ${item.unit.symbol} of ${item.name}'),
+                              );
+                            },
+                          )),
+      ),
     );
   }
 }
